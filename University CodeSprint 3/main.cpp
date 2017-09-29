9a93441e72dea2df036ced47ac6ea0d4d608bdae
YujianLiu <lyj19921022@gmail.com>
2017-09-29 13:59:11 -0400

    Initial Commit

---
+++ University CodeSprint 3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  University CodeSprint 3
+//
+//  Created by yujian liu on 9/29/17.
+//  Copyright © 2017 HackerRank. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
